352a8155ff561b930efdf720d831897c2ea5a5c7
xinguankeli <xinguankeli@outlook.com>
2021-12-26 22:21:52 +0800

    Signed-off-by: xinguankeli <xinguankeli@outlook.com>

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QTextCodec>
+#include "server/server.h"
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF8"));
+    Server *server = new Server(quint16(65534));
+    return a.exec();
+}
